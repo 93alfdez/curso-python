df4c29fd2fab46a6a33a5d3a41451ece0cbccdcf
Álvaro Fernández Sánchez <alvaro_0211@hotmail.com>
2024-05-17 16:50:01 +0200

    ejercicios corregidos

--- Ejercicios/05.py
+++ Ejercicios/05.py
@@ -23,7 +23,7 @@ aritmética (3+5/2*5)2."""
 """5. Escribir un programa que pregunte al usuario por el número de horas trabajadas y el
 coste por hora. Después debe mostrar por pantalla la paga que le corresponde."""
 
-# h , c = int(input("¿Cuantas horas has trabajado? ")) , int(input("¿Cuanto te pagan por hora? "))
+# h , c = int(float(input("¿Cuantas horas has trabajado? "))) , int(float(input("¿Cuanto te pagan por hora? ")))
 # t = (h*c)
 # print(f"Tienen que pagarte {t}")
 
@@ -41,8 +41,8 @@ el índice de masa corporal y lo almacene en una variable, y muestre por pantall
 Tu índice de masa corporal es <imc> donde <imc> es el índice de masa corporal
 calculado redondeado con dos decimales."""
 # p , e = float(input("¿Cual es tu peso? ")) , float(input("¿Cuanto mides? "))
-# imc = (p/e)
-# print(f'Tu índice de masa corporal es {"%.2f" % imc}')
+# imc = round(p/(e**2),2)
+# print(f'Tu índice de masa corporal es imc')
 
 
 """8. Escribir un programa que pida al usuario dos números enteros y muestre por pantalla la división
@@ -58,17 +58,17 @@ entera respectivamente."""
 """9. Escribir un programa que pregunte al usuario una cantidad a invertir, el interés anual y
 el número de años, y muestre por pantalla el capital obtenido en la inversión."""
 
-# c , i, a = int(float(input("¿Qué cantidad quieres invertir? "))), int(float(input("¿A que interés? "))), int(input("¿Cuantos años? "))
-# t = (c*(1+(i/100)))*a
+# c , i, a = float(input("¿Qué cantidad quieres invertir? ")), float(input("¿A que interés? ")), int(input("¿Cuantos años? "))
+# t = round((c*(1+(i/100)))**a, 2)
 
-# print(f"El capital obtenido es de {"%.2f" % t} €")
+# print(f"El capital obtenido es de {t} €")
 
 
 
 """10. Una juguetería tiene mucho éxito en dos de sus productos: payasos y muñecas. Suele hacer venta por correo y la empresa de logística les cobra por peso de cada paquete así que deben calcular el peso de los payasos y muñecas que saldrán en cada paquete a demanda. Cada payaso pesa 112 g y cada muñeca 75 g. Escribir un programa que lea el número de payasos y muñecas vendidos en el último pedido y calcule el peso total del paquete que será enviado."""
 
-# numP = int(input("¿Cuantos payasos quieres comprar? "));
-# numM = int(input("¿Cuantas muñecas quieres comprar? "));
+# numP = int(float(input("¿Cuantos payasos quieres comprar? ")));
+# numM = int(float(input("¿Cuantas muñecas quieres comprar? ")));
 
 # pesoP = 112
 # pesoM = 75
@@ -77,13 +77,17 @@ el número de años, y muestre por pantalla el capital obtenido en la inversión
 
 """11. Imagina que acabas de abrir una nueva cuenta de ahorros que te ofrece el 4% de interés al año. Estos ahorros debido a intereses, que no se cobran hasta finales de año, se te añaden al balance final de tu cuenta de ahorros. Escribir un programa que comience leyendo la cantidad de dinero depositada en la cuenta de ahorros, introducida por el usuario. Después el programa debe calcular y mostrar por pantalla la cantidad de ahorros tras el primer, segundo y tercer años. Redondear cada cantidad a dos decimales."""
 
-# ah = int(float(input("¿Qué cantidad de ahorros tienes? ")));
+# ah = float(input("¿Qué cantidad de ahorros tienes? "));
+# i = 0.04
 
-# y1 = (ah*(1+(4/100)))
-# y2 = (y1*(1+(4/100)))
-# y3 = (y2*(1+(4/100)))
+# n1,n2,n3,n4 = 1,2,3,4
 
-# print(f"Después del primer año la cantidad de ahorros es: {"%.2f" % y1} €\nSegundo año sería: {"%.2f" % y2} €\nTercer año: {"%.2f" % y3} €")
+# y1 = ah*((1+i)**n1), 2
+# y2 = y1*((1+i)**n2),2
+# y3 = y2*((1+i)**n3),2
+# y4 = y3*((1+i)**n4),2
+
+# print(f"Después del primer año la cantidad de ahorros es: {y1:.2f} €\nSegundo año sería: {y2:.2f} €\nTercer año: {y3:.2f} €\nCuarto año: {y4:.2f} €")
 
 """12. Una panadería vende barras de pan a 3.49€ cada una. El pan que no es el día tiene un descuento del 60%. Escribir un programa que comience leyendo el número de barras vendidas que no son del día. Despunés el programa debe mostrar el precio habitual de una barra de pan, el descuento que se le hace por no ser fresca y el coste final total."""
 
